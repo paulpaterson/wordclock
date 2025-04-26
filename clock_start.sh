8af0a9ea2d23656cf518d205adc6fb5cf0ac577f
Paul Paterson <ppaterson@gmail.com>
2025-04-25 22:44:32 -0500

    Adjust script to send the show_a flag

--- clock_start.sh
+++ clock_start.sh
@@ -2,6 +2,6 @@
 
 cd /home/paul/Workspace/wordclock
 source .env/bin/activate
-python terminal_version.py --mode 16x16 --light-mode real --interval 10 --baud-rate 1000 --show-it-is --light-color 255 255 0
+python terminal_version.py --mode 16x16 --light-mode real --interval 10 --baud-rate 1000 --show-it-is --show-a --light-color 255 255 0
 
 
