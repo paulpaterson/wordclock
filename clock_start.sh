ed177086f6d2c510bb9e128cdfd8aaf799e59d47
Paul Paterson <ppaterson@gmail.com>
2025-04-27 21:52:34 -0500

    Added service for UI also

--- clock_start.sh
+++ clock_start.sh
@@ -6,6 +6,7 @@ source .env/bin/activate
 
 exit_status=2
 
+
 until [ $exit_status -ne 2 ]; do
   source ./config.sh
   python terminal_version.py
---
+++ ui_start.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+cd /home/paul/Workspace/wordclock
+
+source .env/bin/activate
+
+# Start the UI configuration
+python ui_backend.py
+
+
