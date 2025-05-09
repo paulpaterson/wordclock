b59cd12c4cb666629ad9706ac78f69bd43ba1917
Paul Paterson <ppaterson@gmail.com>
2025-05-08 21:51:43 -0500

    Added ability to call the national weather service to get a forecast

--- data_update.py
+++ data_update.py
@@ -1,5 +1,5 @@
 import json
-
+import requests
 import aiohttp
 from omnilogic import OmniLogic
 import time
@@ -28,25 +28,70 @@ async def get_pool_data(store):
     store['air'] = status[0]['airTemp']
 
 
+def map_weather_name(name):
+    """Return a simple version of the forecasted weather
+
+    Returns either Sunny, Cloudy, Rainy
+
+    """
+    name = name.lower()
+    if "sunny" in name or "clear" in name:
+        return "Sunny"
+    elif "cloud" in name:
+        return "Cloudy"
+    elif "rain" in name or "shower" in name or "storm" in name:
+        return "Rain"
+
+
+def get_weather_forecast(store):
+    """Return the forecasted weather for the next few hours"""
+    url = "https://api.weather.gov/gridpoints/HGX/31,80/forecast/hourly"
+    response = requests.get(url)
+    response.raise_for_status()
+    forecast_data = response.json()
+    look_ahead = []
+    for period in forecast_data['properties']['periods']:
+        forecast = period['shortForecast']
+        usable_forecast = map_weather_name(forecast)
+        look_ahead.append(usable_forecast)
+    store['forecast'] = look_ahead
+
+
 @click.command()
 @click.option('--interval', type=int, default=5, help='How often to run (seconds)')
-def main(interval):
+@click.option('--pool', is_flag=True, default=False, help='Whether to get pool information')
+@click.option('--weather', is_flag=True, default=False, help='Whether to get weather information')
+@click.option('--debug', is_flag=True, default=False, help='Drop into debug mode when complete')
+@click.option('--iterations', type=int, default=-1, help='How many times to run')
+def main(interval, pool, iterations, weather, debug):
     store = {}
-    while True:
-        asyncio.run(get_pool_data(store))
+    while iterations:
+        if pool:
+            asyncio.run(get_pool_data(store))
+        if weather:
+            weather = get_weather_forecast(store)
+
         print(store)
         with open('local_data.json', 'w') as f:
             data = {
-                'pool-temp': int(store['water']),
-                'air-temp': int(store['air']),
+                'pool-temp': int(store.get('water', -1)),
+                'air-temp': int(store.get('air', -1)),
                 'garage-open': False,
                 'ac-down-auto': True,
-                'ac-up-auto': True
+                'ac-up-auto': True,
+                'forecast': store.get('forecast', [])
             }
             f.write(json.dumps(data, indent=4))
         #
-        time.sleep(interval)
-
+        iterations -= 1
+        if iterations:
+            time.sleep(interval)
+    #
+    if debug:
+        import pdb
+        pdb.set_trace()
 
 if __name__ == "__main__":
     main()
+
+
