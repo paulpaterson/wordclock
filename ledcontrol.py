86f7dea2e22b57dbf3872f61ac8f89bb23fb974d
Paul Paterson <ppaterson@gmail.com>
2025-06-24 16:20:50 -0500

    Added button logic

--- ledcontrol.py
+++ ledcontrol.py
@@ -1,16 +1,33 @@
 
 import gpiozero
 import time
+import signal
 
 i = 0
 
-led = gpiozero.LED(17)
+#led = gpiozero.LED(17)
 
-while i < 100:
-    led.on()
-    time.sleep(1)
-    led.off()
-    time.sleep(0.1)
-    i += 1
+#while i < 100:
+#    led.on()
+#    time.sleep(1)
+#    led.off()
+#    time.sleep(0.1)
+#    i += 1
+
+b = gpiozero.Button(2)
+print('Waiting for the button to be pressed')
+#b.wait_for_press()
+#print('The button was pressed')
+
+def on():
+    print('Button is on')
+
+def off():
+    print('Button is off')
+
+b.when_pressed = on
+b.when_released = off
+
+signal.pause()
 
 
