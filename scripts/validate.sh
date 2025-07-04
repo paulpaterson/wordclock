210a0471a3c837863be49f8ddb003e0956936318
Paul Paterson <ppaterson@gmail.com>
2025-07-04 14:24:14 -0500

    Added validation logic script

---
+++ scripts/validate.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+
+# Script to validate the configuration of the RPI
+# to see if everything should be working.
+
+R="\033[31m"
+G="\033[32m"
+Y="\033[33m"
+W="\033[0m"
+
+printf "\nChecking overall configuration for errors.\n\n"
+
+# SPIDEV enabled
+SPI=`sudo raspi-config nonint get_spi`
+if [ $SPI -eq 0 ]; then
+  printf "$G - SPI looks to be enabled\n"
+else
+  printf "$R - SPI doesn't seem to be enabled - clock will not be able to control lights\n"
+fi
+
+printf "$W"
+
+# HWC enabled
+dmesg | grep -q  "rtc-ds1307"
+if [ "$?" -eq 0 ]; then
+  printf "$G - RTC looks to be enabled\n"
+else
+  # No RTC - but maybe we have NTP service
+  sudo timedatectl | grep -q "NTP service: active"
+  if [ "$?" -eq 0 ]; then
+    printf "$Y - No RTC but NTP is active so clock will be OK as long as it has WIFI connection\n"
+  else
+    printf "$R - RTC doesn't seem to be enabled or did not start and NTP is not set - clock will be wrong after a power cut\n"
+  fi
+fi
+
+printf "$W"
+
+
+# Boot behaviour
+BOOT=`sudo raspi-config nonint get_autologin`
+if [ $BOOT -eq 1 ]; then
+  printf "$G - Booting into the command line\n"
+else
+  printf "$R - Not booting into the command line - may cause issues or slowness\n"
+fi
+
+printf "$W"
+
+# Services
+CLOCK=`systemctl is-active  clock-startup-script.service`
+if [ "$CLOCK" == "active" ]; then
+  printf "$G - Clock main service is active\n"
+else
+  printf "$R - Clock service 'clock-startup-script.service' is not active - clock will not be running\n"
+fi
+
+UI=`systemctl is-active  clock-ui-script.service`
+if [ "$UI" == "active" ]; then
+  printf "$G - Clock UI front end service is active\n"
+else
+  printf "$R - Clock UI front end service 'clock-ui-script.service' is not active - will not be able to use web front end\n"
+fi
+
+
+printf "$W"
+
+printf "\nChecks completed\n\n"
