ac060b9e20a9cc5d6438b13e8e198e566b9a70b3
Paul Paterson <ppaterson@gmail.com>
2025-07-04 11:22:39 -0500

    Fixed command line parameters for buttons

--- clock_start.sh
+++ clock_start.sh
@@ -9,7 +9,7 @@ exit_status=2
 
 until [ $exit_status -ne 2 ]; do
   source ./config.sh
-  python terminal_version.py --button-pin 27 --set-system-time 
+  python terminal_version.py --button-pin 27 --mode-button-pin 25 --set-system-time 
 
   # Get the exit status of the last command
   exit_status=$?
