0de61c24bf56968445cb01f63e6e835a4252bb71
Paul Paterson <ppaterson@gmail.com>
2025-08-28 16:46:22 -0500

    Fixed path in test

--- test/test_utils.py
+++ test/test_utils.py
@@ -1,10 +1,12 @@
 import pytest
 import subprocess
 import re
+import pathlib
 
 def test_get_ip_address():
     """Check we can get an IP address from the script"""
-    result = subprocess.run(['../scripts/getip.sh'], capture_output=True)
+    path = pathlib.Path(__file__).parent.parent / 'scripts' / 'getip.sh'
+    result = subprocess.run([path], capture_output=True)
     ip = result.stdout.decode('utf-8').strip()
     assert re.match('\d+\.\d+\.\d+\.\d+', ip)
     assert result.returncode == 0
